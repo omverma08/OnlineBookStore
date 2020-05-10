54aeb33b429e475c725014570f1f491fa0f24af3
omverma08 <omrverma@gmail.com>
2020-05-10 16:44:24 +0530

    uploading all file

---
+++ OnlinebookStore/src/main/java/com/inmemory/onlinebookstore/OnlinebookStoreApplication.java
@@ -0,0 +1,14 @@
+package com.inmemory.onlinebookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinebookStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinebookStoreApplication.class, args);
+		System.out.println("hello-world");
+	}
+
+}
---
+++ OnlinebookStore/src/test/java/com/inmemory/onlinebookstore/OnlinebookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inmemory.onlinebookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinebookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
